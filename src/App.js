77b5d4c034437ab5eec1c9d7a3ffed84970f5506
sajeeb5506 <sajeebsarker5506@gmail.com>
2022-03-26 00:32:03 +0600

    data fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
+import Shop from './Components/Shop/Shop';
 
 function App() {
+  const [books,setBooks]=useState([]);
+
+  useEffect(()=>{
+    fetch('books.json')
+    .then(res=>res.json())
+    .then(data=>setBooks(data));
+
+  },[]);
+
+
   return (
     <div className="">
-   
+      <Shop></Shop>
     </div>
   );
 }
---
+++ src/Components/Shop/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Shop.css';
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+        <h2> Your Favourite Books</h2>
+        <h3> Lest Goo...</h3>
+        </div>
+    );
+};
+
+export default Shop;
