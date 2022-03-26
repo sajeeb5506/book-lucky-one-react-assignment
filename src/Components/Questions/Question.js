242876ab072244b07ec2ec0b1a0b46fb2d930193
sajeeb5506 <sajeebsarker5506@gmail.com>
2022-03-26 10:28:18 +0600

    question add

--- src/Components/Questions/Question.js
+++ src/Components/Questions/Question.js
@@ -4,12 +4,18 @@ const Question = () => {
     return (
         <div className='question'>
             <h1>How react Work?</h1>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus, tempora.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui velit, laudantium laboriosam adipisci minus vero tempore accusantium debitis iusto. Quasi.
+            <p> While building client-side apps, a team of Facebook developers realized that the DOM is slow (The Document Object Model (DOM) is an application programming interface (API) for HTML and XML documents. It defines the logical structure of documents and the way a document is accessed and manipulated.). So, to make it faster, React implements a virtual DOM that is basically a DOM tree representation in JavaScript. So when it needs to read or write to the DOM, it will use the virtual representation of it. Then the virtual DOM will try to find the most efficient way to update the browser’s DOM.
+            Unlike browser DOM elements, React elements are plain objects and are cheap to create. React DOM takes care of updating the DOM to match the React elements. The reason for this is that JavaScript is very fast and it’s worth keeping a DOM tree in it to speed up its manipulation.
+            Although React was conceived to be used in the browser, because of its design it can also be used in the server with Node.js. 
             </p>
-            <h1>How react Work?</h1>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus, tempora.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui velit, laudantium laboriosam adipisci minus vero tempore accusantium debitis iusto. Quasi.
+            <h1>Props vs State in React</h1>
+            <p>
+               <h2>  State Vs. Props</h2>
+                  <h2> State:</h2>
+                       The state is an updatable structure that is used to contain data or information about the component and can change over time. The change in state can happen as a response to user action or system event. It is the heart of the react component which determines the behavior of the component and how it will render. A state must be kept as simple as possible. It represents the component's local state or information. It can only be accessed or modified inside the component or by the component directly.<br/>
+
+                   <h2> Props:</h2>
+                       Props are read-only components. It is an object which stores the value of attributes of a tag and work similar to the HTML attributes. It allows passing data from one component to other components. It is similar to function arguments and can be passed to the component the same way as arguments passed in a function. Props are immutable so we cannot modify the props from inside the component.
             </p>
             
         </div>
